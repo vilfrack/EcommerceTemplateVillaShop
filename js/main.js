99436dedbbbe42c8e11b66486aa9fd20366096e5
vilfrack <vilfrack21@gmail.com>
2015-06-24 12:43:20 -0500

    Template Ecommerce
    v1.0

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//carrusel
+    $(document).ready(function () {
+        $("#owl-demo").owlCarousel({
+            navigation: true
+        });
+    });
+    $(document).ready(function () {
+
+        $("#owl-demo2").owlCarousel({
+            navigation: true
+        });
+
+    });
+    $(document).ready(function () {
+
+        $("#owl-demo3").owlCarousel({
+            navigation: true
+        });
+
+    });
+//jquery del menu
+    $(document).ready(function ($) {
+
+        $('#mega-1').dcVerticalMegaMenu({
+            rowItems: '3',
+            speed: 'fast',
+            effect: 'show',
+            direction: 'right'
+        });
+
+    });
