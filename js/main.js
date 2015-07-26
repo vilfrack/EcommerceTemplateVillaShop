e62dcc011c7752262b7628c56878b509556e2b90
vilfrack <vilfrack21@gmail.com>
2015-07-26 13:47:17 -0500

    Se crea la nueva ventana de listas productos v1.2
    Se agrego la nueva ventana de listas productos, asi mismo el plugic para
    el rango de precios

--- js/main.js
+++ js/main.js
@@ -29,3 +29,11 @@
         });
 
     });
+/*price range*/
+
+ $('#sl2').slider();
+
+	var RGBChange = function() {
+	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+	};	
+		
